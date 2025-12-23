b72be7ea29ca3b6dc50f4a27beec36439674c291
Romano3D <romanodez@gmail.com>
2025-12-23 14:11:45 -0300

    add scripts aproveitados

---
+++ Assets/Scripts/Items/ItemCollactableBase.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemCollactableBase : MonoBehaviour
+{
+    public string compareTag = "Player";
+    public ParticleSystem particleVFX;
+    public float timeToHide = 3;
+    public GameObject graphicItem;
+
+    [Header("Sounds")]
+    public AudioSource audioSource;
+
+  private void Awake()
+    {
+        //if (particleVFX != null) particleVFX.transform.SetParent(null);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+    }
+    protected virtual void Collect()
+    {
+        if (graphicItem != null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
+        OnCollect();
+    }
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+    protected virtual void OnCollect()
+    {
+        if (particleVFX != null) particleVFX.Play();
+        if (audioSource != null) audioSource.Play();
+    }
+
+}
+ 
+
+   
+
+
+
+
+
+    
+
---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ebac.Core.Singleton;
+
+public class ItemManager : Singleton<ItemManager>
+{ 
+    public SOint coins;
+    public SOint coinsSpecial;
+
+    public TextMeshProUGUI uiTextcoins;
+    public TextMeshProUGUI uiTextcoinsSpecial;
+    private void Start()
+    {
+        Reset();
+    }
+    private void Reset()
+    {
+        coins.value = 0;
+        coinsSpecial.value = 0;
+        UpdateUI();
+    }
+    public void AddCoins(int amount = 1)
+    {
+        coins.value += amount;
+        UpdateUI();
+    }
+    public void AddCoinsSpecial(int amount = 3)
+    {
+        coinsSpecial.value += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        //uiTextcoins.text = coins.value.ToString();
+        //uiTextcoinsSpecial.text = coinsSpecial.value.ToString();
+        //UIInGameManager.UpdateTexCoins(coins.ToString());
+       // UIInGameManager.UpdateTexCoins(coinsSpecial.ToString());
+    }
+}
