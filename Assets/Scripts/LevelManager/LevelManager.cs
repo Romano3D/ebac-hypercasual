52574d219dffdeee5516fa127d9dd5fa512e8c1b
Romano3D <romanodez@gmail.com>
2026-02-03 10:55:30 -0300

    Add script Start e End pieces

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,12 +10,26 @@ public class LevelManager : MonoBehaviour
 
     public List<GameObject> levels;
 
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+
+    public int piecesStartNumber = 3;
+    public int piecesNumber = 5;
+    public int piecesEndNumber = 1;
+
+    public float timeBetweenPieces = .3f;
+
     [SerializeField] private int _index;
     private GameObject _currentLevel;
 
+    private List<LevelPieceBase> _spawnedPieces;
+
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -38,6 +53,52 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    #region
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesStartNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+        for (int i = 0; i < piecesEndNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+    }
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+
+    }
+
+    #endregion
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
